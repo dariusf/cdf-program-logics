ea5c727b60d99cf7f09eaf9cc06ad3fe986e2220
Darius Foo <darius.foo.tw@gmail.com>
2024-02-23 13:23:26 +0800

    Try to model programs, deal-breaking mismatch

--- Flow.v
+++ Flow.v
@@ -39,131 +39,236 @@ Ltac destr H :=
 
 Module Flow3.
 
-Inductive flow : Type :=
-| req : precond -> flow
-| ens : postcond -> flow
-| seq : flow -> flow -> flow
-.
+  Definition ident := string.
+  Definition val := Z.
 
-Infix ";;" := seq (at level 80, right associativity).
+  Inductive expr : Type :=
+    | pvar (x: ident)
+    | pconst (n: val)
+    | plet (x: ident) (e1: expr) (e2: expr)
+    | pref (x: ident)
+    | pderef (x: ident)
+    | passign (x1: ident) (x2: ident)
+    | pif (x: ident) (e1: expr) (e2: expr)
+    .
 
-Inductive result : Type :=
-| norm : Z -> result
-.
+  Definition store : Type := ident -> Z.
+  Definition sempty : store := (fun _ => -1).
+  Definition supdate (x: ident) (v: Z) (s: store) : store :=
+    fun y => if string_dec x y then v else s y.
 
-(* axiomatization of semantics for staged formulae *)
-Inductive satisfies : bool -> heap -> bool -> heap -> result -> flow -> Prop :=
-  | sem_req: forall h3 p h1 r,
-    (exists h2, h3 = hunion h1 h2 /\ hdisjoint h1 h2 /\ p h2) ->
-    satisfies true h3 true h1 (norm r) (req p)
-  | sem_ens: forall h1 h3 q v,
-    (exists h2, h3 = hunion h1 h2 /\ hdisjoint h1 h2 /\
-    q v h2) ->
-    satisfies true h1 true h3 (norm v) (ens q)
-  | sem_seq: forall f1 f2 r r1 h1 h2 h3 c,
-    satisfies true h1 true h2 r f1 ->
-    satisfies true h2 c h3 r1 f2 ->
-    satisfies true h1 c h3 r1 (f1;;f2)
-.
+  Inductive eresult : Type :=
+    | enorm : val -> eresult
+  .
 
-(* forward rules say how to produce a staged formula from a program *)
-Inductive forward : com -> flow -> Prop :=
-  | fw_skip:
-    forward SKIP (ens (fun res => (res = 0) //\\ emp))
-  | fw_pure: forall n,
-    forward (PURE n) (ens (fun res => (res = n) //\\ emp))
-  | fw_get: forall l v, 
-    forward (GET l)
-      (req (contains l v) ;;
-      (ens (fun r => (r = v) //\\ contains l v)))
-.
+  Inductive bigstep : store -> heap -> expr -> store -> heap -> eresult -> Prop :=
+    | eval_pvar : forall s h x,
+      bigstep s h (pvar x) s h (enorm (s x))
+    | eval_pconst : forall s h x,
+      bigstep s h (pconst x) s h (enorm x)
+    | eval_plet : forall x e1 e2 v s h h2 s2 s1 h1 r,
+      bigstep s h e1 s1 h1 (enorm v) ->
+      bigstep (supdate x v s1) h1 e2 s2 h2 r ->
+      bigstep s h (plet x e1 e2) s h2 r
+    | eval_pref : forall x s (h:heap) l,
+      h l = None ->
+      bigstep s h (pref x) s (hupdate l (s x) h) (enorm l)
+    | eval_deref : forall x s (h:heap) v,
+      h (s x) = Some v ->
+      bigstep s h (pderef x) s h (enorm v)
+    | eval_assign : forall x1 x2 s h,
+      bigstep s h (passign x1 x2) s (hupdate (s x1) (s x2) h) (enorm 0)
+  .
 
-Definition terminated (c: com) : Prop :=  c = SKIP.
+  Module ProgramExamples.
 
-Definition terminates (c1: com) (h1 h2: heap) : Prop :=
-  exists c2, star red (c1, h1) (c2, h2) /\ terminated c2.
+    Example ex_ref :
+      bigstep
+      sempty hempty
+      (plet "x" (pconst 1) (pref "x"))
+      sempty
+      (hupdate 2 1 hempty)
+      (enorm 2).
+    Proof.
+      apply eval_plet with (v:=1) (s1:=sempty) (s2:=supdate "x" 1 sempty) (h1:=hempty).
+      apply eval_pconst.
+      apply eval_pref.
+      constructor.
+    Qed.
 
-Theorem soundness : forall h1 c h3 r f,
-  satisfies true h1 c h3 r f ->
-  forall e1 h2,
-  forward e1 f ->
-  terminates e1 h1 h2 ->
-  h2 = h3.
-Proof.
-  intros h1 c h3 r f Hs.
-  induction Hs; intros e1 h0 Hf Ht.
-  - inv Hf.
-  - { inv Hf.
-    (* both ways to produce an ens: skip and pure *)
-    - inv Ht.
-      inv H.
-      destr H1. destruct H3. unfold emp in H3. inv H0.
-      (* skip has terminated *)
-      { inv H4.
-      - heap.
+  End ProgramExamples.
+
+  Inductive sl : Type :=
+  | pts : ident -> ident -> sl
+  | sep : sl -> sl -> sl
+  .
+
+  Inductive sl_satisfies : store -> heap -> sl -> Prop :=
+  | sl_pts : forall s h x y,
+    contains (s x) (s y) h ->
+    sl_satisfies s h (pts x y)
+  (* | sl_sep : forall s h x y,
+    contains (s x) (s y) h ->
+    sl_satisfies s h (sep p q) *)
+  .
+
+  (* Definition precond := sl. *)
+  (* Definition postcond := sl. *)
+
+  Inductive flow : Type :=
+  (* | req : precond -> flow *)
+  (* | ens : postcond -> flow *)
+  | req : precond -> flow
+  | ens : postcond -> flow
+  | seq : flow -> flow -> flow
+  | fexists : (Z -> flow) -> flow -> flow
+  .
+
+  Infix ";;" := seq (at level 80, right associativity).
+
+  Inductive result : Type :=
+  | norm : Z -> result
+  .
+
+  (* axiomatization of semantics for staged formulae *)
+  Inductive satisfies : bool -> heap -> bool -> heap -> result -> flow -> Prop :=
+    | sem_req: forall h3 p h1 r,
+      (exists h2, h3 = hunion h1 h2 /\ hdisjoint h1 h2 /\ p h2) ->
+      satisfies true h3 true h1 (norm r) (req p)
+    | sem_ens: forall h1 h3 q v,
+      (exists h2, h3 = hunion h1 h2 /\ hdisjoint h1 h2 /\
+      q v h2) ->
+      satisfies true h1 true h3 (norm v) (ens q)
+    | sem_seq: forall f1 f2 r r1 h1 h2 h3 c,
+      satisfies true h1 true h2 r f1 ->
+      satisfies true h2 c h3 r1 f2 ->
+      satisfies true h1 c h3 r1 (f1;;f2)
+  .
+
+    (* forward rules say how to produce a staged formula from a program *)
+    Inductive forward : expr -> flow -> Prop :=
+      | fw_const: forall n,
+        forward (pconst n) (ens (fun res => (res = n) //\\ emp))
+      | fw_deref: forall x z,
+        forward (pderef x) (req (contains x z);; ens (fun res => (res = z) //\\ contains x z))
+      (* | fw_get: forall l v, 
+        forward (GET l)
+          (req (contains l v) ;;
+          (ens (fun r => (r = v) //\\ contains l v))) *)
+    .
+
+    (* Theorem soundness : forall h1 c h3 r f,
+      satisfies true h1 c h3 r f ->
+      forall e1 h2,
+      (* forward e1 f -> *)
+      (* bigstep s h e1 h1 h2 -> *)
+      h2 = h3.
+    Proof.
+    Admitted. *)
+
+
+  Module SpecExamples.
+    Example ex_spec_return_anything: exists x,
+      satisfies true hempty true hempty (norm x) (req emp).
+    Proof.
+      exists 1.
+      constructor.
+      auto.
+      exists hempty.
+      repeat split; heap.
+    Qed.
+
+    Example ex_spec_ens_1:
+      satisfies true hempty true hempty (norm 1) (ens (fun r => (r = 1) //\\ emp)).
+    Proof.
+      apply sem_ens.
+      exists hempty.
+      repeat split; heap.
+    Qed.
+
+    Example ex_spec_seq: forall x,
+      satisfies true (hupdate x 1 hempty) true (hupdate x 1 hempty) (norm 2)
+      (req (contains x 1);; ens (fun r => (r = 2) //\\ contains x 1)).
+    Proof.
+      intros.
+      apply sem_seq with (h2 := hempty) (r := norm 3).
+      - constructor.
+      exists (hupdate x 1 hempty).
+      repeat split.
+      heap.
+      heap.
+      - apply sem_ens.
+      exists (hupdate x 1 hempty).
+      repeat split; heap.
+    Qed.
+  End SpecExamples.
+
+  Module Original.
+    (* forward rules say how to produce a staged formula from a program *)
+    Inductive forward : com -> flow -> Prop :=
+      | fw_skip:
+        forward SKIP (ens (fun res => (res = 0) //\\ emp))
+      | fw_pure: forall n,
+        forward (PURE n) (ens (fun res => (res = n) //\\ emp))
+      | fw_get: forall l v, 
+        forward (GET l)
+          (req (contains l v) ;;
+          (ens (fun r => (r = v) //\\ contains l v)))
+    .
+
+    Definition terminated (c: com) : Prop :=  c = SKIP.
+
+    Definition terminates (c1: com) (h1 h2: heap) : Prop :=
+      exists c2, star red (c1, h1) (c2, h2) /\ terminated c2.
+
+    Theorem soundness : forall h1 c h3 r f,
+      satisfies true h1 c h3 r f ->
+      forall e1 h2,
+      forward e1 f ->
+      terminates e1 h1 h2 ->
+      h2 = h3.
+    Proof.
+      intros h1 c h3 r f Hs.
+      induction Hs; intros e1 h0 Hf Ht.
+      - inv Hf.
+      - { inv Hf.
+        (* both ways to produce an ens: skip and pure *)
+        - inv Ht.
+          inv H.
+          destr H1. destruct H3. unfold emp in H3. inv H0.
+          (* skip has terminated *)
+          { inv H4.
+          - heap.
+          - inv H.
+          }
+        - inv Ht. destr H. destr H0.
+          { inv H3.
+          - destruct H4. unfold emp in H0. subst. heap.
+          - inv H0. 
+          }
+      }
+      - inv Hf.
+      (* only one case for forward rules that produces a seq, which is get *)
+      inv Ht.
+      destr H.
+      (* see how get reduces *)
+      { inv H0.
+      - inv H1. (* get does reduce *)
+    - inv H.
+      { inv H2.
+      - inv H1.
+        (* h0 -> h2 -> h3 *)
+        inv Hs1. destr H0. inv Hs2. destr H4. subst.
+        unfold contains in H3.
+        destruct H7.
+        unfold contains in H6.
+        subst.
+        reflexivity.
       - inv H.
       }
-    - inv Ht. destr H. destr H0.
-      { inv H3.
-      - destruct H4. unfold emp in H0. subst. heap.
-      - inv H0. 
-      }
-  }
-  - inv Hf.
-  (* only one case for forward rules that produces a seq, which is get *)
-  inv Ht.
-  destr H.
-  (* see how get reduces *)
-  { inv H0.
-  - inv H1. (* get does reduce *)
-  - inv H.
-    { inv H2.
-    - inv H1.
-      (* h0 -> h2 -> h3 *)
-      inv Hs1. destr H0. inv Hs2. destr H4. subst.
-      unfold contains in H3.
-      destruct H7.
-      unfold contains in H6.
-      subst.
-      reflexivity.
-    - inv H.
     }
-  }
-(* Admitted. *)
-Qed.
-
-Example ex_spec_return_anything: exists x,
-  satisfies true hempty true hempty (norm x) (req emp).
-Proof.
-  exists 1.
-  constructor.
-  auto.
-  exists hempty.
-  repeat split; heap.
-Qed.
-
-Example ex_spec_ens_1:
-  satisfies true hempty true hempty (norm 1) (ens (fun r => (r = 1) //\\ emp)).
-Proof.
-  apply sem_ens.
-  exists hempty.
-  repeat split; heap.
-Qed.
-
-Example ex_spec_seq: forall x,
-  satisfies true (hupdate x 1 hempty) true (hupdate x 1 hempty) (norm 2)
-  (req (contains x 1);; ens (fun r => (r = 2) //\\ contains x 1)).
-Proof.
-  intros.
-  apply sem_seq with (h2 := hempty) (r := norm 3).
-  - constructor.
-  exists (hupdate x 1 hempty).
-  repeat split.
-  heap.
-  heap.
-  - apply sem_ens.
-  exists (hupdate x 1 hempty).
-  repeat split; heap.
-Qed.
+    (* Admitted. *)
+    Qed.
+  End Original.
 
 End Flow3.
